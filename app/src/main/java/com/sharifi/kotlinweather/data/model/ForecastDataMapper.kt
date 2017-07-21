4a5e35a20c3601e382bdcd7a561b16c4f79ed287
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2017-07-21 17:34:00 +0430

    separate data models and app models and create a data mapper to convert them. create command interface and implement it to get ForecastList result

---
+++ app/src/main/java/com/sharifi/kotlinweather/data/model/ForecastDataMapper.kt
@@ -0,0 +1,34 @@
+package com.sharifi.kotlinweather.data.model
+
+import com.sharifi.kotlinweather.data.repository.Forecast
+import com.sharifi.kotlinweather.data.repository.ForecastResult
+import java.text.DateFormat
+import java.util.*
+import com.sharifi.kotlinweather.data.model.Forecast as ModelForecast
+/**
+ * Created by sharifi on 7/21/17.
+ */
+public class ForecastDataMapper {
+
+    fun convertFromDataModel(forecastResult: ForecastResult): ForecastList {
+        return ForecastList(forecastResult.city.name, forecastResult.city.country, convertToForecastList(forecastResult.list))
+    }
+
+    private fun convertToForecastList(list: List<Forecast>) : List<ModelForecast> {
+        return list.map { convertToModelForecast(it) }
+    }
+
+    private fun convertToModelForecast(forecast: Forecast): ModelForecast {
+        return ModelForecast(
+                convertDate(forecast.dt),
+                forecast.weather[0].description,
+                forecast.temp.max.toInt(),
+                forecast.temp.min.toInt()
+        )
+    }
+
+    private fun convertDate(date: Long): String {
+        val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
+        return df.format(date * 1000)
+    }
+}
