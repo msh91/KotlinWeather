17966102a144fa4cbad0aaad789e21bc95010367
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2017-07-21 17:58:08 +0430

    update recyclerview adapter after getting data from api

--- app/src/main/java/com/sharifi/kotlinweather/home/ForecastListAdapter.kt
+++ app/src/main/java/com/sharifi/kotlinweather/home/ForecastListAdapter.kt
@@ -3,12 +3,13 @@ package com.sharifi.kotlinweather.home
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
+import com.sharifi.kotlinweather.data.model.ForecastList
 
 /**
  * Created by mohammad on 7/19/17.
  */
 
-class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -16,10 +17,13 @@ class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<Foreca
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tv.text = items[position]
+        with(weekForecast.dailyForecast[position]) {
+
+            holder.tv.text = "$date - $description - $high/$low"
+        }
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = weekForecast.dailyForecast.size
 
     class ViewHolder(val tv: TextView) : RecyclerView.ViewHolder(tv)
 
