d93039e35ee229245f0bf20d162dfd2eab150bf1
Mohammad Sharifi <sharifi.m91@gmail.com>
2017-08-04 17:36:38 +0430

    use kotlin android extention to find views automatically

--- app/src/main/java/com/sharifi/kotlinweather/data/model/AppModels.kt
+++ app/src/main/java/com/sharifi/kotlinweather/data/model/AppModels.kt
@@ -7,8 +7,8 @@ data class ForecastList(
         val city: String,
         val country: String,
         val dailyForecast: List<Forecast>) {
-    operator fun get(position: Int): Forecast = dailyForecast[position]
-    fun size(): Int = dailyForecast.size
+    operator fun get(position: Int) = dailyForecast[position]
+    fun size() = dailyForecast.size
 }
 
 data class Forecast(
--- app/src/main/java/com/sharifi/kotlinweather/home/ForecastsFragment.kt
+++ app/src/main/java/com/sharifi/kotlinweather/home/ForecastsFragment.kt
@@ -1,20 +1,17 @@
 package com.sharifi.kotlinweather.home
 
-import android.annotation.TargetApi
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
 import com.sharifi.kotlinweather.R
 import com.sharifi.kotlinweather.data.Person
 import com.sharifi.kotlinweather.data.commands.RequestForecastCommand
+import kotlinx.android.synthetic.main.fragment_home.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
@@ -33,13 +30,11 @@ class ForecastsFragment : Fragment() {
             "Sun 6/29 - Sunny - 20/7"
     )
     private val TAG = ForecastsFragment::class.java.simpleName
-    private lateinit var forecastList: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val root = inflater!!.inflate(R.layout.fragment_home, container, false)
 
-        forecastList = root.findViewById(R.id.forecast_list) as RecyclerView
         forecastList.layoutManager = LinearLayoutManager(context)
 
         return root
@@ -63,7 +58,7 @@ class ForecastsFragment : Fragment() {
             val forecastResult = RequestForecastCommand("Tehran").execute()
             Log.d(TAG, forecastResult.toString())
             uiThread {
-                forecastList.adapter = ForecastListAdapter(forecastResult) {forecast ->  showToast(forecast.date) }
+                forecastList.adapter = ForecastListAdapter(forecastResult) { forecast -> showToast(forecast.date) }
             }
         }
     }
