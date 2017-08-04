e43a4a948fb95d70936f0b1b9c9955063b08c97d
Mohammad Sharifi <sharifi.m91@gmail.com>
2017-08-04 23:29:03 +0430

    create Application class with a singleton instance that delegates it's initialization to a custom delegate class

---
+++ app/src/main/java/com/sharifi/kotlinweather/util/CustomDelegate.kt
@@ -0,0 +1,26 @@
+package com.sharifi.kotlinweather.util
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * Created by sharifi on 8/4/17.
+ */
+private class NotNullSingleValueVar<T> : ReadWriteProperty<Any?, T> {
+    private var value: T? = null
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return value ?: throw IllegalStateException("${property.name} " +
+                "not initialized")
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = if (this.value == null) value
+        else throw IllegalStateException("${property.name} already initialized")
+    }
+}
+
+object CustomDelegate {
+    fun <T> notNullSingleValue():
+            ReadWriteProperty<Any?, T> = NotNullSingleValueVar()
+}
