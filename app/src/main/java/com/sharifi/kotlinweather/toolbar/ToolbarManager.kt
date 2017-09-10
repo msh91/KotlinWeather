fb62c045a50548d730504a6c34c5d8430995932d
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2017-09-10 15:09:08 +0430

    create Settings Activity and a delegate that responses for zipCode

--- app/src/main/java/com/sharifi/kotlinweather/toolbar/ToolbarManager.kt
+++ app/src/main/java/com/sharifi/kotlinweather/toolbar/ToolbarManager.kt
@@ -6,8 +6,10 @@ import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import com.sharifi.kotlinweather.App
 import com.sharifi.kotlinweather.R
+import com.sharifi.kotlinweather.setting.SettingsActivity
 import com.sharifi.kotlinweather.util.slideEnter
 import com.sharifi.kotlinweather.util.slideExit
+import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
 /**
@@ -25,7 +27,7 @@ interface ToolbarManager {
         toolbar.inflateMenu(R.menu.menu_main)
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
-                R.id.action_settings -> App.instance.toast("Settings")
+                R.id.action_settings -> toolbar.context.startActivity<SettingsActivity>()
                 else -> App.instance.toast("Unknown Action")
             }
             true
