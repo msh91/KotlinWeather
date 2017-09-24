fc146833c5c977ee26e0092bfc20e2333e16a2dc
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2017-09-25 00:39:43 +0330

    create api service and rest client to create api services

--- app/src/main/java/com/sharifi/kotlinweather/data/repository/ForecastServerRepository.kt
+++ app/src/main/java/com/sharifi/kotlinweather/data/repository/ForecastServerRepository.kt
@@ -2,12 +2,15 @@ package com.sharifi.kotlinweather.data.repository
 
 import com.sharifi.kotlinweather.data.model.Forecast
 import com.sharifi.kotlinweather.data.model.ForecastList
+import com.sharifi.kotlinweather.data.service.ApiService
+import com.sharifi.kotlinweather.data.service.RestClient
 
 /**
  * Created by sharifi on 9/24/17.
  */
 class ForecastServerRepository: ForecastRepository {
     override fun requestForecastByZipCode(zipCode: Long, date: Long): ForecastList? {
+        val apiService: ApiService = RestClient.createService(ApiService::class.java)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
