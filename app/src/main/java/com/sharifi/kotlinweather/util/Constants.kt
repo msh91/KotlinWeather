7bc9fda470564767218c42aee3f863f57cce7255
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2017-10-04 00:28:30 +0330

    change constants class to object

--- app/src/main/java/com/sharifi/kotlinweather/util/Constants.kt
+++ app/src/main/java/com/sharifi/kotlinweather/util/Constants.kt
@@ -3,9 +3,7 @@ package com.sharifi.kotlinweather.util
 /**
  * Created by sharifi on 9/24/17.
  */
-class Constants {
-    companion object {
-        val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
-        val BASE_URL: String = "http://api.openweathermap.org/"
-    }
+object Constants {
+    val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
+    val BASE_URL: String = "http://api.openweathermap.org/"
 }
