2062d3617b0bd8324f5f20a7fc726c32d00d0aeb
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2017-10-04 17:27:49 +0330

    create base contracts (View and Presenter) for mvp architecture

--- app/src/main/java/com/sharifi/kotlinweather/base/BaseFragment.kt
+++ app/src/main/java/com/sharifi/kotlinweather/base/BaseFragment.kt
@@ -1,20 +1,47 @@
 package com.sharifi.kotlinweather.base
 
+import android.content.Context
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.View
 
 /**
  * Created by sharifi on 10/4/17.
  */
-abstract class BaseFragment : Fragment() {
-    var canCommitFragmentTransactions = false
+abstract class BaseFragment : Fragment(), BaseView {
+    override var fragmentTransactionCanBeCommitted: Boolean = false
+    override val ctx: Context by lazy { context }
+    abstract val presenter: BasePresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter.onCreate()
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.onViewCreated()
+    }
 
     override fun onStart() {
         super.onStart()
-        canCommitFragmentTransactions = true
+        fragmentTransactionCanBeCommitted = true
+        presenter.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.onResume()
     }
 
     override fun onStop() {
         super.onStop()
-        canCommitFragmentTransactions = false
+        fragmentTransactionCanBeCommitted = false
+        presenter.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.onDestroy()
     }
 }
