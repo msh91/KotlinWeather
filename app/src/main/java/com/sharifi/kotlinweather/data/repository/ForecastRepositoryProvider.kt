2e6ebed60cc6587de4d2a1e01cdab9412d4ee4bd
Mohammad Sharifi <sharifi.m91@gmail.com>
2017-10-11 18:13:13 +0330

    implement ForecastRepositoryProvider: first attempts to get result from db repository, if request fails then try to get data from server repository

--- app/src/main/java/com/sharifi/kotlinweather/data/repository/ForecastRepositoryProvider.kt
+++ app/src/main/java/com/sharifi/kotlinweather/data/repository/ForecastRepositoryProvider.kt
@@ -3,17 +3,21 @@ package com.sharifi.kotlinweather.data.repository
 import com.sharifi.kotlinweather.data.model.Forecast
 import com.sharifi.kotlinweather.data.model.ForecastList
 import com.sharifi.kotlinweather.data.repository.db.ForecastDbRepository
+import com.sharifi.kotlinweather.data.repository.server.ForecastServerRepository
 import com.sharifi.kotlinweather.data.repository.server.service.RestError
 
 /**
  * Created by sharifi on 9/24/17.
  */
-class ForecastRepositoryProvider(val sources: List<ForecastRepository> = SOURCES): ForecastRepository {
+class ForecastRepositoryProvider(val sources: List<ForecastRepository> = SOURCES) : ForecastRepository {
     companion object {
-        val SOURCES = listOf(ForecastDbRepository())
+        val SOURCES = listOf(ForecastDbRepository(), ForecastServerRepository())
     }
+
     override fun requestForecastByZipCode(zipCode: Long, date: Long, success: (ForecastList) -> Unit, failure: (RestError) -> Unit) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        sources[0].requestForecastByZipCode(zipCode, date, { success(it) }, {
+            sources[1].requestForecastByZipCode(zipCode, date, success, failure)
+        })
     }
 
     override fun requestDayForecast(id: Long, success: (Forecast) -> Unit, failure: (RestError) -> Unit) {
