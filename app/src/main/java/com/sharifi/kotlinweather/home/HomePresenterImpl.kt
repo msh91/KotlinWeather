6cd5c9504fc4286c1924e973af3b0452dc99626b
Mohammad Sharifi <sharifi.m91@gmail.com>
2017-10-11 15:08:24 +0330

    use generic type to declare baseView instance in basePresenter

--- app/src/main/java/com/sharifi/kotlinweather/home/HomePresenterImpl.kt
+++ app/src/main/java/com/sharifi/kotlinweather/home/HomePresenterImpl.kt
@@ -3,5 +3,6 @@ package com.sharifi.kotlinweather.home
 /**
  * Created by sharifi on 10/10/17.
  */
-class HomePresenterImpl: HomePresenter {
+class HomePresenterImpl(override val mView: HomeView) : HomePresenter{
+
 }
