85f9a98a95fc8c4e1bc6b62bc6afad2f3e89703c
Mohammad Sharifi <sharifi.m91@gmail.com>
2017-11-17 23:20:11 +0330

    remove legacy files and clean project

--- app/src/main/java/com/sharifi/kotlinweather/App.kt
+++ app/src/main/java/com/sharifi/kotlinweather/App.kt
@@ -1,14 +1,13 @@
 package com.sharifi.kotlinweather
 
 import android.app.Application
-import com.sharifi.kotlinweather.legacy.util.CustomDelegate
 
 /**
  * Created by sharifi on 8/4/17.
  */
 class App : Application() {
     companion object {
-        var instance: App by CustomDelegate.notNullSingleValue()
+        lateinit var instance: App
     }
 
     override fun onCreate() {
